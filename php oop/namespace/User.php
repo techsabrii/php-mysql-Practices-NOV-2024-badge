ad086e3d8f829d72f9c720147ee6e4aa79a40683
TechSabrii <bk83835@gmail.com>
2025-07-24 13:36:52 +0500

    a

--- php oop/namespace/User.php
+++ php oop/namespace/User.php
@@ -23,3 +23,24 @@ class User {
         return "User Name: {$this->name}, Email: {$this->email}";
     }
 }
+class Address {
+    private $name;
+    private $email;
+
+    public function __construct($name, $email) {
+        $this->name = $name;
+        $this->email = $email;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function getDetails() {
+        return "User Name: {$this->name}, Email: {$this->email}";
+    }
+}
--- php oop/namespace/get.php
+++ php oop/namespace/get.php
@@ -4,11 +4,15 @@ require 'User.php';
 
 use StudentNamespace\Student;
 use UserNamespace\User;
+use UserNamespace\Address;
 
 $student = new Student("Ali", 20);
-$userDetails = new Student("Abdul Basit", 25);
+$userDetails = new Student("Abdul-Basit", 25);
 $user = new User("Basit", "basit@example.com");
+$address = new Address("Basit", "basit@example.com");
 
 echo $student->getDetails() . "<br>";
 echo $userDetails->getDetails() . "<br>";
+echo $userDetails->getAge() . "<br>";
 echo $user->getDetails(). "<br>";
+echo $address->getDetails(). "<br>";
